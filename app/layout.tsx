05da94322220d919620872c1d14e9971bcf18cf1
tymey <tyler.meyer.a@gmail.com>
2025-04-07 21:30:36 -0500

    build: Set up tailwind for Next.js site

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,12 @@
 import React from "react";
+import type { Metadata } from 'next';
+
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'Tyler Meyer Dev',
+  description: 'Tyler Meyer\'s Developer Portfolio'
+}
 
 export default function RootLayout({
   children,
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default function Page() {
-  return <h1>Hello World!</h1>
+  return (
+    <h1 className="text-3xl font-bold underline">Hello World!</h1>
+  );
 }
