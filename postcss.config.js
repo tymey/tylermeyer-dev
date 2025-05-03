7efbcf1534c62127fb61aae349c2bb0f45a5d9de
TYLER MEYER <tylermeyer@mac.mynetworksettings.com>
2025-05-03 16:42:54 -0500

    build: TypeScript & Tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   plugins: {
-    '@tailwindcss/postcss': {},
+    tailwindcss: {},
+    autoprefixer: {},
   },
-}
+};
