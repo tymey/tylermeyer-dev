887ffd4e1c049c83bd20f0d4f2496a43699d895f
tymey <tyler.meyer.a@gmail.com>
2025-05-27 11:35:53 -0500

    feat(hero): Create hero title for portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Welcome from "./Welcome";
 import Hello from "./Hello";
+import Home from "./Paths/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Welcome />} />
+        <Route path="/" element={<Home />} />
         <Route path="/hello" element={<Hello />} />
       </Routes>
     </BrowserRouter>
---
+++ src/Pages/Hero.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { m, LazyMotion, domAnimation } from 'framer-motion';
+
+import HeroText from '../Components/HeroText';
+
+function Hero() {
+    return (
+        <div id="hero" className="w-full flex justify-center overflow-hidden-web relative">
+            <LazyMotion features={domAnimation} strict>
+                <m.div
+                    id="hero"
+                    className="relative w-full flex justify-center items-center h-screen min-h-[800px]"
+                >
+                    <HeroText />
+                </m.div>
+            </LazyMotion>
+        </div>
+    );
+}
+
+export default Hero;
