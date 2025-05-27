232828cd70e20553a709f0c667f846a254bc8785
tymey <tyler.meyer.a@gmail.com>
2025-05-27 10:07:14 -0500

    build: Configure tailwind with primary/grayscale colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}", "./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        green: '0 0 10rem #29C26F'
+      },
+    },
+    colors: {
+      primary: {
+        200: '#FFFFFF',
+        400: '#B64D80',
+        500: '#CD6899',
+        600: '#99235C',
+      },
+      grayscale: {
+        25: '#E4E2DD',
+        50: '#CD6899',
+        100: '#EFEFEF',
+        200: '#FDFDFF',
+        950: '#823A5D',
+      },
+    },
   },
   plugins: [],
 };
