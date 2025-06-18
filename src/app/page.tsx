ec85d1418e2482fc2d863667cbc21a13d08bc348
tymey <tyler.meyer.a@gmail.com>
2025-06-18 00:40:48 -0500

    fix(build): Remove @ aliases

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 "use client"
 
-import Hero from "@/sections/Hero";
-import About from "@/sections/About";
-import Projects from "@/sections/Projects";
+import Hero from "../sections/Hero";
+import About from "../sections/About";
+import Projects from "../sections/Projects";
 
 export default function Page() {
     return (
