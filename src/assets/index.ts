444670961acc352882fe691edf80917d6aa0c481
tymey <tyler.meyer.a@gmail.com>
2025-06-17 23:57:11 -0500

    feat(projects): Update assets project logos

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,18 @@
+// Tech
+import githubIcon from "./tech/github.png";
+
+// Project Logos
+import flareLogo from "./projects/flareLogo.png";
+import gameKnightLogo from "./projects/gameKnightLogo.png";
+import vitalityLogo from "./projects/vitalityLogo.png";
+
+// Profile Pic
 import profile from "./profile/profile.png";
 
 export {
+    githubIcon,
+    flareLogo,
+    gameKnightLogo,
+    vitalityLogo,
     profile,
 };
