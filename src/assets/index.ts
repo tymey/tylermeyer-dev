82500dcac00e75bf08d2b8573cbf8988421ea446
tymey <tyler.meyer.a@gmail.com>
2025-06-19 00:32:15 -0500

    feat(skills): Add tech logo pngs

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,33 @@
 // Tech
+import agileIcon from "./tech/agile.png";
+import awsIcon from "./tech/aws.png";
+import axiosIcon from "./tech/axios.png";
+import cIcon from "./tech/c.png";
+import cronIcon from "./tech/cron.png";
+import cssIcon from "./tech/css.png";
+import expressIcon from "./tech/expressjs.png";
+import gitIcon from "./tech/git.png";
 import githubIcon from "./tech/github.png";
+import githubActionsIcon from "./tech/githubactions.png";
+import htmlIcon from "./tech/html.png";
+import javascriptIcon from "./tech/javascript.png";
+import jestIcon from "./tech/jest.png";
+import materialUIIcon from "./tech/materialui.png";
+import mongoDBIcon from "./tech/mongodb.png";
+import mongoDBAtlasIcon from "./tech/mongodbatlas.png";
+import mongooseIcon from "./tech/mongoosejs.png";
+import mysqlIcon from "./tech/mysql.png";
+import nextjsIcon from "./tech/nextjs.png";
+import nodejsIcon from "./tech/nodejs.png";
+import passportIcon from "./tech/passportjs.png";
+import pythonIcon from "./tech/python.png";
+import reactIcon from "./tech/reactjs.png";
+import reactRouterIcon from "./tech/reactrouter.png";
+import sequelizeIcon from "./tech/sequelize.png";
+import sqlIcon from "./tech/sql.png";
+import tailwindIcon from "./tech/tailwindcss.png";
+import typescriptIcon from "./tech/typescript.png";
+import vercelIcon from "./tech/vercel.png";
 
 // Project Logos
 import flareLogo from "./projects/flareLogo.png";
@@ -10,7 +38,35 @@ import vitalityLogo from "./projects/vitalityLogo.png";
 import profile from "./profile/profile.png";
 
 export {
+    agileIcon,
+    awsIcon,
+    axiosIcon,
+    cIcon,
+    cronIcon,
+    cssIcon,
+    expressIcon,
+    gitIcon,
     githubIcon,
+    githubActionsIcon,
+    htmlIcon,
+    javascriptIcon,
+    jestIcon,
+    materialUIIcon,
+    mongoDBIcon,
+    mongoDBAtlasIcon,
+    mongooseIcon,
+    mysqlIcon,
+    nextjsIcon,
+    nodejsIcon,
+    passportIcon,
+    pythonIcon,
+    reactIcon,
+    reactRouterIcon,
+    sequelizeIcon,
+    sqlIcon,
+    tailwindIcon,
+    typescriptIcon,
+    vercelIcon,
     flareLogo,
     gameKnightLogo,
     vitalityLogo,
