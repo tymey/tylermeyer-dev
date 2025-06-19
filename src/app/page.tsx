b5489c9beb888d829b9b2f4c57eb4212d0a5e29a
tymey <tyler.meyer.a@gmail.com>
2025-06-18 21:39:49 -0500

    feat(skills): Create Skills section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,15 @@
 import Hero from "@/sections/Hero";
 import About from "@/sections/About";
 import Projects from "@/sections/Projects";
+import Skills from "@/sections/Skills";
 
 export default function Page() {
     return (
         <>
-            <Hero />
-            <About />
-            <Projects />
+            <Hero /> {/* Big Intro */}
+            <About /> {/* More Detailed Intro */}
+            <Projects /> {/* Project Cards */}
+            <Skills /> {/* Skill Cards */}
         </>
     );
 }
