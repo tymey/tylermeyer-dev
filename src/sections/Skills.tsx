b612bd0128eb4c8673d57b4d00d425e34bd359f7
tymey <tyler.meyer.a@gmail.com>
2025-06-18 23:20:38 -0500

    feat(skills): Add section title for skills

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -1,6 +1,14 @@
+import SectionTitle from "@/components/SectionTitle";
+
 function Skills() {
     return (
-        <div>Skills</div>
+        <div id="skills" className="w-full overflow-hidden-web flex justify-center">
+            <div className="w-full min-h-[800px] flex flex-col xl:w-[70%]">
+                <div className="w-full">
+                    <SectionTitle title="SKILLS" subtitle="What I can do"/>
+                </div>
+            </div>
+        </div>
     );
 }
 
