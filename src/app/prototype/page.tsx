065af9196d627eb75c4b50f023289ce39d87eea7
tymey <tyler.meyer.a@gmail.com>
2025-06-22 01:56:53 -0500

    feat(codewars): Prototype codewars component

---
+++ src/app/prototype/page.tsx
@@ -0,0 +1,79 @@
+"use client"
+
+import { channel } from "diagnostics_channel";
+import { useEffect, useState } from "react";
+
+type CodewarsData = {
+    language: string;
+    count: number;
+    completedChallenges: [string, string],
+}[];
+
+export default function Page() {
+    const [codeWarsData, setCodeWarsData] = useState<CodewarsData>([]);
+
+    const fetchCodeWars = () => {
+        fetch("https://www.codewars.com/api/v1/users/tymey/code-challenges/completed?page=0")
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! state ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Completed Challenges:', data);
+                const langData = data.data.reduce((acc: any, curr: any) => {
+                    curr.completedLanguages.forEach((lang: any) => {
+                        if (!acc[lang]) {
+                            acc[lang] = {
+                                count: 0,
+                                completedChallenges: [],
+                            }
+                        }
+                        acc[lang].count++;
+                        acc[lang].completedChallenges.push([curr.name, `https://www.codewars.com/kata/${curr.id}`]);
+                    });
+                    return acc;
+                }, {});
+
+                console.log('Reformatted Data:', langData);
+                
+                const langArray = Object.getOwnPropertyNames(langData).map(lang => (
+                    {
+                        language: lang,
+                        count: langData[lang].count,
+                        completedChallenges: langData[lang].completedChallenges,
+                    }
+                ));
+
+                setCodeWarsData(langArray);
+            })
+            .catch(error => {
+                console.error("Error fetching Codewars data:", error);
+            });
+    };
+
+    useEffect(() => {
+        fetchCodeWars();
+    }, []);
+
+    console.log('Code Wars Data:', codeWarsData);
+
+    return (
+        <>
+            <div className="flex items-center justify-center mt-20">
+                {codeWarsData.map((lang, index) => (
+                    <div key={`${index}-${lang.language}`}>
+                        <h1 className="text-4xl">{`${lang.language.toUpperCase()}: ${lang.count}`}</h1>
+                        {lang.completedChallenges.map((challenge, index) => (
+                            <p key={`${index}-${challenge[0]}`}>
+                                {challenge[0]}
+                                <a href={challenge[1]} target="_blank" className="hover:underline">{` Go to Challenge`}</a>
+                            </p>
+                        ))}
+                    </div>
+                ))}
+            </div>
+        </>
+    );
+}
